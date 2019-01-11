9205c6d5d026dc239db40a9217e4fa087e4ea7c6
julianofritz <julianofritz@users.noreply.github.com>
2019-01-11 14:12:18 -0200

    Revert "teste 2"
    This reverts commit 4b9e2bf6834a5f50884eae95499b640e970e1be9.

--- public/index.php
+++ public/index.php
@@ -17,8 +17,6 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-$dir;
-
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
